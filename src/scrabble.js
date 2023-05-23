aab6e057823adb01830cbd7ca53065430c48fc57
Divy <2011492@brunel.ac.uk>
2023-05-23 15:00:26 +0100

    Completed the scrabble challenge- with a another solution to meed the extended criteria

--- src/scrabble.js
+++ src/scrabble.js
@@ -51,6 +51,22 @@ class Scrabble {
           score += 10;
           break;
       }
+
+      // THIS BELOW IS A BETTER VERSION
+      // const characterValues = {
+      //   'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+      //   'D': 2, 'G': 2,
+      //   'B': 3, 'C': 3, 'M': 3, 'P': 3,
+      //   'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+      //   'K': 5,
+      //   'J': 8, 'X': 8,
+      //   'Q': 10, 'Z': 10
+      // }
+
+      // for (let i = 0; i < this.word.legth; i++){
+      //   const character = this.word[i];
+      //   score += characterValues[character] || 0;
+      // }
     }
     return score;
   }
