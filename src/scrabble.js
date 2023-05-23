f900f91e5a01df9726b0d78a0049ff39091bc28b
Divy <2011492@brunel.ac.uk>
2023-05-23 14:33:17 +0100

    Completed the scrabble challenge

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,60 @@
 class Scrabble {
+  constructor(input) {
+    this.input = input ? input.toUpperCase() : '';
+  }
 
   score() {
-    // Write your implementation here
+    let score = 0;
+
+    for (let i = 0; i < this.input.length; i++) {
+      let character = this.input[i];
+
+      switch (character) {
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+        case 'L':
+        case 'N':
+        case 'R':
+        case 'S':
+        case 'T':
+          score += 1;
+          break;
+        case 'D':
+        case 'G':
+          score += 2;
+          break;
+        case 'B':
+        case 'C':
+        case 'M':
+        case 'P':
+          score += 3;
+          break;
+        case 'F':
+        case 'H':
+        case 'V':
+        case 'W':
+        case 'Y':
+          score += 4;
+          break;
+        case 'K':
+          score += 5;
+          break;
+        case 'J':
+        case 'X':
+          score += 8;
+          break;
+        case 'Q':
+        case 'Z':
+          score += 10;
+          break;
+      }
+    }
+    return score;
   }
 }
 
 export default Scrabble;
+
